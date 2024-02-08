b9fb52f7b7cc5ee199e53306f8fe7143e5258ebd
Rachit <rachitshrm96@gmail.com>
2024-02-09 01:51:42 +0530

    api cors debug

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,11 +1,21 @@
 import axios from 'axios';
 
-// const API = axios.create({baseURL:'http://localhost:5000'});
-const API = axios.create({baseURL:'https://captureapp.onrender.com'});
+// const API = axios.create({
+//    baseURL:'http://localhost:5000',
+//    withCredentials: true,
+// });
+const API = axios.create({
+   baseURL:'https://captureapp.onrender.com',
+   withCredentials: true,
+});
 
 API.interceptors.request.use((req)=>{
-   if(localStorage.getItem('profile')){
-    req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`
+   const user = JSON.parse(localStorage.getItem('profile'));
+   const token = user?.token;
+   console.log('Token (Interceptor):', token);
+
+   if (token) {
+     req.headers.Authorization = `Bearer ${token}`;
    }
    return req;
 })
